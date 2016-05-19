d5496075099cefbafc7f72b77d9a33ce0c45995b
Robert Weclawski <robert.weclawski@mykolab.com>
2016-05-20 01:43:08 +0200

    init

---
+++ src/kata/Controller.java
@@ -0,0 +1,8 @@
+package kata;
+
+public class Controller {
+
+    public int add(int a, int b){
+        return (a + b);
+    }
+}
---
+++ src/test/kata/ControllerTest.java
@@ -0,0 +1,25 @@
+package test.kata;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class ControllerTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        assertFalse(true);
+    }
+
+} 
